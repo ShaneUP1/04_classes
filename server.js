2ff056fbb0e51d1d8046b94f832183cab391f557
Shane Upchurch <shaneupchurch@gmail.com>
2020-12-03 16:39:32 -0600

    server connected

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const app = express();
+const Stack = require('./stack/stack.js');
 
+app.use(express.json());
 
+//endpoints
+// app.post('/lint'), (req, res) => {
+
+// }
+
+app.get('/lint', (req, res) => {
+    console.log(req.body);
+    res.send('hey there');
+});
+
+// listen
+app.listen(3000, () => {
+    console.log('started on 3000');
+});
